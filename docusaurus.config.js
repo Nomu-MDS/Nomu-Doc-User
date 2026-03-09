5481ecbea5246f467d440590e785ee6c48a3da79
Crmy7 <114735818+Crmy7@users.noreply.github.com>
2026-03-09 13:45:14 +0100

    fix: disable respectPrefersColorScheme in color mode configuration and update hero image

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -88,7 +88,7 @@ const config = {
       colorMode: {
         defaultMode: 'light',
         disableSwitch: false,
-        respectPrefersColorScheme: true,
+        respectPrefersColorScheme: false,
       },
       navbar: {
         title: '',
